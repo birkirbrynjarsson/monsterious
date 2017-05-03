0bbaf7181517451393ff0678876bf38b74330798
Unnur Sól Ingimarsdóttir <unnuri15@ru.is>
2017-05-03 18:07:30 +0000

    Spawn works

--- Assets/Scripts/GameController/SpawnMonsters.cs
+++ Assets/Scripts/GameController/SpawnMonsters.cs
@@ -18,16 +18,48 @@ public class SpawnMonsters : MonoBehaviour {
         float[] floors = new float[7] {floor1, floor2, floor3, floor4, floor5, floor6, floor7};
 
         // TODO: Check if there is a monster on the floor... if so then spawn next to the monster.
-        for(int i = 0; i<1; i++)
+        for(int i = 0; i < 3; i++)
         {
             System.Random rand = new System.Random();
             int randomIndex = rand.Next(0, floors.Length);
             float randomFloor = floors[randomIndex];
-
-            GameObject green = GameObject.Find("greenMonster");
+            GameObject floor = GameObject.Find("Floors/Floor" + (randomIndex + 1));
+            //float randomFloor = floors[6];
+            GameObject green = (GameObject)Resources.Load("greenMonster");
+                //GameObject.Find("Floors/Floor7/greenMonster");
+            
 
             // TODO: Check if there is a monster on the floor... if so then spawn next to the monster.
-            Instantiate(green, new Vector2(2, (randomFloor+0.42f)), Quaternion.identity);
+            
+            float x = 0.44f;
+            float y = (randomFloor + 0.42f);
+            for (int j = floor.transform.childCount; j > 0; j--)
+            {
+                x = x + 1f;
+            }
+            Instantiate(green, new Vector2(x, y), Quaternion.identity).transform.parent = floor.transform;
+
+            //    Debug.Log("Hey! There is a monster there!");
+            //    Instantiate(green, new Vector2(1.20f, (randomFloor + 0.42f)), Quaternion.identity);
+            //}
+            //else
+            //{
+            //    Debug.Log("Alot of space!!");
+            //    Instantiate(green, new Vector2(0.44f, (randomFloor + 0.42f)), Quaternion.identity);
+
+            //}
+
+            //if (GameObject.FindWithTag("Monster").transform.position == new Vector3(0.44f, (randomFloor + 0.42f)))
+            //{
+            //    Debug.Log("Hey! There is a monster there!");
+            //    Instantiate(green, new Vector2(1.20f, (randomFloor + 0.42f)), Quaternion.identity);
+            //}
+            //else
+            //{
+            //    Debug.Log("Alot of space!!");
+            //    Instantiate(green, new Vector2(0.44f, (randomFloor + 0.42f)), Quaternion.identity);
+            //}
+
         }
     }
 	
