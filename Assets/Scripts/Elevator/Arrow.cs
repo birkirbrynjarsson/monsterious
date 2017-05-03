48f34560a04841646cc6490c04e3a7a655651982
sigrung15 <sigrung15@ru.is>
2017-05-03 15:28:20 +0000

    tiny elevator movement

--- Assets/Scripts/Elevator/Arrow.cs
+++ Assets/Scripts/Elevator/Arrow.cs
@@ -11,6 +11,33 @@ public class Arrow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.touchCount > 0)
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit2D hit;
+            Touch myTouch = Input.touches[0];
+
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
+                if (hit.collider != null && hit.transform.gameObject.tag == "Arrow")
+                {
+                    Debug.Log("SUCCESS" + hit.transform.gameObject.name);
+                }
+                if (hit.transform.gameObject.name == "ArrowDownRight")
+                {
+                    GameObject elevator = GameObject.Find("ElevatorRight");
+                    Vector3 pos = elevator.transform.position;
+                    pos.y = 1;
+                    iTween.MoveTo(elevator, pos, 1);
+                    /* elevator.transform.position = Vector3.MoveTowards(
+                        elevator.transform.position, 
+                        pos, 
+                        0.1 * Time.deltaTime);*/
+                    // elevator.transform.position = pos;
+                }
+            }
+
+        }
+    }
 }
