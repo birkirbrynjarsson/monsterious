538c3f8d6de67050de8d4a962edab7a479b215b8
Unnur Sól Ingimarsdóttir <unnuri15@ru.is>
2017-05-04 18:52:58 +0000

    Spawn Patience

--- Assets/Scripts/Monsters/Patience.cs
+++ Assets/Scripts/Monsters/Patience.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Patience : MonoBehaviour {
 
     public Transform LoadingBar;
-    //public Transform TextIndicator;
+    public Transform TextIndicator;
     //public Transform TextLoading;
     [SerializeField] private float currentAmount;
     [SerializeField] private float speed;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        //TODO: Find random number from 1-7 and put to floor
+        System.Random rand = new System.Random();
+        int desiredFloor = rand.Next(1, 7);
+        Debug.Log(desiredFloor);
+        TextIndicator.GetComponent<Text>().text = desiredFloor.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
         if(currentAmount < 100)
         {
@@ -25,10 +30,12 @@ public class Patience : MonoBehaviour {
             //TextIndicator.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
             //TextLoading.gameObject.SetActive(true);
         }
-        else
+        else // 100%
         {
             //TextLoading.gameObject.SetActive(true);
             //TextIndicator.GetComponent<Text>().text = "DONE!";
+            
+            //TODO: Let the monster know that it should leave.. and the Patience meter will drop 
         }
         LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
     }
