37bbeaf33d1c0cb2f977b004984a7d8c6fbe7a9b
sigrung15 <sigrung15@ru.is>
2017-05-04 10:23:49 +0000

    elevator masterpiese

---
+++ Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GUIText scoreText;
+    private static int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        UpdateScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddScore(int newScoreValue) {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    static void UpdateScore () {
+        scoreText.text = " " + score;
+    }
+}
